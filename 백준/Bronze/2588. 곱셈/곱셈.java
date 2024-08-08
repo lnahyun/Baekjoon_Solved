b533664812358cbb3c04be3fb0bc2e460de557da
lnahyun <164346626+lnahyun@users.noreply.github.com>
2024-08-08 15:36:30 +0900

    [Bronze III] Title: 곱셈, Time: 104 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int A = Integer.parseInt(br.readLine());
+        String B = br.readLine();
+
+        System.out.println(A * (B.charAt(2) - '0'));
+        System.out.println(A * (B.charAt(1) - '0'));
+        System.out.println(A * (B.charAt(0) - '0'));
+        System.out.println(A * Integer.parseInt((B)));
+
+    }
+}
